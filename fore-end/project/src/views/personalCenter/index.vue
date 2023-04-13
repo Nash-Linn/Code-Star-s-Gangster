dc55939943affbf2faa2090d3afff2300b9bf058
unknown <linnc@asiainfo.com>
2023-04-13 23:03:51 +0800

    update

--- fore-end/project/src/views/personalCenter/index.vue
+++ fore-end/project/src/views/personalCenter/index.vue
@@ -13,26 +13,28 @@ import menus from './components/menus.vue'
 </script>
 <style scoped lang="less">
 .personal-center-wrap {
+  position: relative;
   width: 100%;
   min-height: @main-height;
   padding: @base-padding;
 
-  display: flex;
-
   .left {
+    position: fixed;
     flex-shrink: 0;
     width: 200px;
+    min-height: calc(@main-height - 2 * @base-padding);
     background-color: #fff;
     border-radius: @base-border-radius;
-    margin-right: 20px;
     padding: @base-padding;
   }
 
   .right {
-    width: 100%;
+    width: calc(100% - 220px);
+    min-height: calc(@main-height - 2 * @base-padding);
     background-color: #fff;
     border-radius: @base-border-radius;
     padding: @base-padding;
+    margin-left: 220px;
   }
 }
 </style>
