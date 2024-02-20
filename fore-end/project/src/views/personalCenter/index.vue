4f06b5f7fd1ed89e0b8e230334b7e8719ce47f14
nash <nash0207@163.com>
2024-02-20 17:05:17 +0800

    fix: keep-alive 导致的bug

--- fore-end/project/src/views/personalCenter/index.vue
+++ fore-end/project/src/views/personalCenter/index.vue
@@ -6,9 +6,9 @@
     <div class="right">
       <router-view v-slot="{ Component }">
         <keep-alive>
-          <component :is="Component" v-if="$route.meta.keepAlive" />
+          <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive" />
         </keep-alive>
-        <component :is="Component" v-if="!$route.meta.keepAlive" />
+        <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive" />
       </router-view>
     </div>
   </div>
